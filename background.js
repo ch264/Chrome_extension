10b6e6273985b3462f4b2d4ffe09cb5525b31c57
ch264 <christinavhastenrath@gmail.com>
2019-06-13 16:51:19 -0700

    refractor background.js and added style.css

--- background.js
+++ background.js
@@ -1,47 +1,55 @@
 
-console.log('js');
-
-chrome.browserAction.onClicked.addListener(function() {
-	chrome.tabs.create({'url': "chrome://newtab"})
-})
-
-
+///////////////////////////////////////////////////////
+// /////////// Functions //////////////////////////////
+///////////////////////////////////////////////////////
 // opens the settings form
 function openSettings() {
 	document.getElementById('settings').classList.toggle('settings-open');
 	console.log('in button')
 }
 
-document.getElementById('button-settings').addEventListener('click', openSettings)
-
-
-let userName = localStorage.getItem('receivedName');
-
+// saves userName to localStorage
 function saveName() {
 	localStorage.setItem('receivedName', userName)
 }
 
+// sets userName equal to localStorage
+let userName = localStorage.getItem('receivedName');
+
+// If no userName is set, display friend
 if (userName == null) {
 	userName = "friend";
 }
 
+// capture form input and save in variable userName
 function changeName() {
 	userName = document.getElementById('input-name').value;
 	saveName();
 }
 
-
-
-document.getElementById("button-name").addEventListener('click', function(){
-
-	console.log('in add button')
-	changeName();
-	
-})
-
-
+// displays userName in greeting
 function getGreeting() {
 	document.getElementById('greeting').innerHTML = `Why, hello there ${userName}. You got this!`;
 }
 
+///////////////////////////////////////////////////////
+// /////////// EventListeners /////////////////////////
+///////////////////////////////////////////////////////
+
+
+chrome.browserAction.onClicked.addListener(function() {
+	chrome.tabs.create({'url': "chrome://newtab"})
+})
+
+document.getElementById('button-settings').addEventListener('click', openSettings)
+
+
+
+// On click on add button, change the userName
+document.getElementById("button-name").addEventListener('click', function(){
+	changeName();
+})
+
+
+// calls the userName display function
 getGreeting();
