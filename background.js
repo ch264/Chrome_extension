6674a8a2c32b677be4d711e0f4d63222d6811f8c
ch264 <christinavhastenrath@gmail.com>
2019-06-14 15:24:29 -0700

    chrome extension ready to be published

--- background.js
+++ background.js
@@ -1,3 +1,6 @@
+console.log(`<div>Icons made by <a href="https://www.freepik.com/" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>`)
+
+
 //////////////////////////////////////////////////////
 // /////////// Variables //////////////////////////////
 ///////////////////////////////////////////////////////
@@ -79,10 +82,10 @@ function getGreeting() {
 		return quotes[Math.floor(Math.random() * quotes.length)];
 	}
 
-	window.onload = function() {
-		let randomQuote = quote(quotes);
-		document.getElementById('quote').innerHTML = randomQuote;
-	}
+	// window.onload = function() {
+	// 	let randomQuote = quote(quotes);
+	// 	document.getElementById('quote').innerHTML = randomQuote;
+	// }
 
 
 
